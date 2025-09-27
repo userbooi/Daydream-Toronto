6a8bba4fe5afb679617405c4f5f2453090721f2b
Edwin Wen <edwinwen75@gmail.com>
2025-09-27 16:26:13 -0400

    fixed conflicts

--- Scripts/guns.gd
+++ Scripts/guns.gd
@@ -2,12 +2,7 @@ extends Node2D
 
 @export var bullet: PackedScene
 @export var firerate: float
-
-<<<<<<< HEAD
 @export var fire = true
-
-=======
->>>>>>> fd3067d19631e60178072626b339e2af0acbe40a
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	change_gun()
@@ -26,7 +21,6 @@ func handle_inputs():
 			else:
 				Game.gun_num = 0
 			change_gun()
-<<<<<<< HEAD
 	if Game.gun_num != 1 and Game.gun_num != 3:
 		if Input.is_action_just_pressed("fire") and fire:
 			fire = false
@@ -37,10 +31,6 @@ func handle_inputs():
 			fire = false
 			shoot()
 			$Timer.start()
-=======
-	if Input.is_action_just_pressed("fire"):
-		shoot()
->>>>>>> fd3067d19631e60178072626b339e2af0acbe40a
 
 #@export var speed: float
 #@export var damage: int
@@ -58,105 +48,59 @@ func shoot():
 	bullet_instance.maxrange = Game.gun_stats[Game.gun_names[Game.gun_num]][2]
 	bullet_instance.knockback = Game.gun_stats[Game.gun_names[Game.gun_num]][3]
 	get_node("/root/Main/Projectiles").add_child(bullet_instance)
-<<<<<<< HEAD
 
 func detect_angle():
 	if (get_global_mouse_position().x < Game.player.position.x):
 		$Sprite2D.scale = Vector2(1, -1)
 		position = Vector2(-5,0)
 	else:
-=======
-	#$bullets.add_child(bullet_instance)
-
-func detect_angle():
-	if (get_global_mouse_position().x < Game.player.position.x):
-		#$Sprite2D.flip_v = true
-		$Sprite2D.scale = Vector2(1, -1)
-		position = Vector2(-5,0)
-		#$shootPoint.position.y = 
-	else:
-		#$Sprite2D.flip_v = false
->>>>>>> fd3067d19631e60178072626b339e2af0acbe40a
 		$Sprite2D.scale = Vector2(1, 1)
 		position = Vector2(5, 0)
+
+		
 				
 func change_gun():
-<<<<<<< HEAD
 	var img = Game.crosshair_sprites[Game.gun_names[Game.gun_num]].get_image()
 	img.resize(img.get_width() * 3, img.get_height() * 3, Image.INTERPOLATE_NEAREST)  # scale ×2
-=======
-	var offsetVector
-	var img = Game.crosshair_sprites[Game.gun_names[Game.gun_num]].get_image()
-	img.resize(img.get_width() * 2, img.get_height() * 2, Image.INTERPOLATE_NEAREST)  # scale ×2
->>>>>>> fd3067d19631e60178072626b339e2af0acbe40a
 	var scaled_tex := ImageTexture.create_from_image(img)
 	
 	if Game.gun_num == 0:
 		
 		$Sprite2D.position = Vector2(3.8, -1.2)
-<<<<<<< HEAD
-=======
-		offsetVector = Vector2(0, 40)
->>>>>>> fd3067d19631e60178072626b339e2af0acbe40a
 		$Sprite2D/shootPoint.position = Vector2(9.8, -3.9)
 		
 	elif Game.gun_num == 1:
 		
 		$Sprite2D.position = Vector2(4.533, -1.2)
-<<<<<<< HEAD
-=======
-		offsetVector = Vector2(20, 12)
->>>>>>> fd3067d19631e60178072626b339e2af0acbe40a
 		$Sprite2D/shootPoint.position = Vector2(14.1, -1.8)
 		
 	elif Game.gun_num == 2:
 		
 		$Sprite2D.position = Vector2(9.6, -1.2)
-<<<<<<< HEAD
-=======
-		offsetVector = Vector2(20, 12)
->>>>>>> fd3067d19631e60178072626b339e2af0acbe40a
 		$Sprite2D/shootPoint.position = Vector2(22, -4.4)
 		
 	elif Game.gun_num == 3:
 		
 		$Sprite2D.position = Vector2(6.467, -1.2)
-<<<<<<< HEAD
-=======
-		offsetVector = Vector2(20, 12)
->>>>>>> fd3067d19631e60178072626b339e2af0acbe40a
 		$Sprite2D/shootPoint.position = Vector2(17, -4)
 		
 	elif Game.gun_num == 4:
 		
 		$Sprite2D.position = Vector2(4.267, -1.2)
-<<<<<<< HEAD
 		$Sprite2D/shootPoint.position = Vector2(12.6, -2.6)
-=======
-		offsetVector = Vector2(20, 12)
-		$shootPoint.position = Vector2(12.6, -2.6)
->>>>>>> fd3067d19631e60178072626b339e2af0acbe40a
 		
 	else:
 		
 		$Sprite2D.position = Vector2(8.3, -1.2)
-<<<<<<< HEAD
 		$Sprite2D/shootPoint.position = Vector2(14, -2.4)
-=======
-		$shootPoint.position = Vector2(14, -2.4)
->>>>>>> fd3067d19631e60178072626b339e2af0acbe40a
 		
 	$Sprite2D.texture = Game.gun_sprites[Game.gun_names[Game.gun_num]]
 	var hotspot = scaled_tex.get_size()/2
 	Input.set_custom_mouse_cursor(scaled_tex, Input.CURSOR_ARROW, hotspot)
 	firerate = Game.gun_stats[Game.gun_names[Game.gun_num]][4]
-<<<<<<< HEAD
 	$Timer.wait_time = firerate
 	
 
 
 func _on_timer_timeout() -> void:
 	fire = true
-=======
-	
->>>>>>> fd3067d19631e60178072626b339e2af0acbe40a
