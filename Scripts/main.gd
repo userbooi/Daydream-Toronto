534abb9911dfe58075ce84569f7fe31150fd7a31
Jayden <153332084+zFa3@users.noreply.github.com>
2025-09-28 14:58:28 -0400

    add edwin

--- Scripts/main.gd
+++ Scripts/main.gd
@@ -1,41 +1,63 @@
 extends Node2D
+signal sacrifice
+signal next_wave
 
-var radius = 150
-var point = 0
+@onready var enemy = preload("res://Scenes/zombie.tscn")
+
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	pass
-
+	$ColorRect.self_modulate.a = 0
+	
+	$enemySpawnTimer.wait_time = Game.spawn_time[Game.curr_level]
+	$enemySpawnTimer.start()
+	
+	connect_signals()
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-<<<<<<< Updated upstream
-	pass
-=======
 	check_inputs()
 	check_enemy_left()
-	if ($Player.position.x ** 2 + $Player.position.y ** 2) ** 0.5 < radius:
-		point += 1
-	print(point)
 
 func check_inputs():
-	if Input.is_action_just_pressed("sacrifice"):
+	if Input.is_action_just_pressed("sacrifice") and Game.player.curr_state == Game.player.STATES.ALIVE:
 		sacrifice.emit()
 		
 func spawn_enemy(pos):
 	var new_enemy = enemy.instantiate()
 	new_enemy.position = pos
+	if new_enemy.position.x < -2050 or new_enemy.position.x > 1000 or new_enemy.position.y < -500 or new_enemy.position.y > 830:
+		return
 	new_enemy.y_sort_enabled = true
 	new_enemy.z_index = 0
-	$Entities.add_child(new_enemy)
+	if check_spawn_area(pos):
+		$Entities.add_child(new_enemy)
+		Game.curr_enemy_num += 1
+
+func check_spawn_area(spawn_pos) -> bool:
+	var space_state = get_world_2d().direct_space_state
+	
+	# Create a small "check shape" (circle here, could also use a rectangle)
+	var shape = CircleShape2D.new()
+	shape.radius = 16  # adjust to enemy size
+	
+	var query = PhysicsShapeQueryParameters2D.new()
+	query.shape = shape
+	query.transform = Transform2D(0, spawn_pos)
+	query.collision_mask = 3  # adjust to match layers you want to avoid
+	
+	var result = space_state.intersect_shape(query)
+	
+	if result.size() == 0:
+		return true
+	else:
+		return false
 
 func _on_enemy_spawn_timer_timeout() -> void:
 	if Game.curr_enemy_num < Game.enemies[Game.curr_level]:
 		var spawn_path: PathFollow2D = $Player.get_node("enemySpawner/PathFollow2D")
 		spawn_path.progress_ratio = randf()
 		spawn_enemy(spawn_path.global_position)
-		Game.curr_enemy_num += 1
 	else:
 		$enemySpawnTimer.stop()
 	
@@ -50,12 +72,15 @@ func connect_signals():
 	connect("next_wave", Callable($CanvasLayer/Sanity, "_add_sanity"))
 	
 func _dim_lights():
-	$WorldEnvironment/PointLight2D.energy += 0.1
+	#pass
+	$WorldEnvironment/PointLight2D.energy += 0.07
 	
 func redo_lights():
+	#pass
 	$WorldEnvironment/PointLight2D.energy = 0.8
 	
 func _start_death():
+	$enemySpawnTimer.stop()
 	for node in get_node("Entities").get_children():
 		node.freeze = true
 		node.linear_velocity = Vector2.ZERO
@@ -69,6 +94,7 @@ func _start_death():
 		node.queue_free()
 	Game.player.linear_velocity = Vector2.ZERO
 	$Player/AnimatedSprite2D.play("death")
+	$CanvasLayer/AnimationPlayer.play("start")
 	
 func check_enemy_left():
 	if Game.enemy_killed == Game.enemies[Game.curr_level]:
@@ -87,6 +113,3 @@ func next_level():
 	else:
 		print("WIN")
 	
-	
-	
->>>>>>> Stashed changes
